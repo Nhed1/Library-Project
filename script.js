34fe7b12a0de9d61831d965ded9aae3f3bd9d428
Leonardo Garreto <leonardogarreto@gmail.com>
2021-12-06 12:15:57 -0300

    Added remove buttons to the bookDiv

--- script.js
+++ script.js
@@ -6,15 +6,36 @@ function Book() {
     this.title = document.querySelector('#title').value
     this.author = document.querySelector('#author').value
     this.pages = document.querySelector('#pages').value
+
 }
 
 function addBookToLibrary() {
     let book = new Book;
     library.push(book)
+    CreateDiv()
 }
 
 function CreateDiv() {
+    let divBook = document.createElement('div'),
+        removeButton = document.createElement('button')
+    removeButton.textContent = 'Remover'
+    // add text to the divs -----------------
+    let title = document.createElement('p')
+    title.textContent = this.title.value
+    divBook.appendChild(title)
 
+    let author = document.createElement('p')
+    author.textContent = this.author.value
+    divBook.appendChild(author)
+
+    let pages = document.createElement('p')
+    pages.textContent = this.pages.value
+    divBook.appendChild(pages)
+
+    divBook.appendChild(removeButton)
+    // add the book to the shelf
+    divBook.classList.add('book')
+    shelf.appendChild(divBook)
 }
 
 
