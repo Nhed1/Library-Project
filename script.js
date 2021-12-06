cac355b2ed04f3f58b64eee61c03599de01c8080
Leonardo Garreto <leonardogarreto@gmail.com>
2021-12-06 11:50:06 -0300

    Created object prototype of the book

--- script.js
+++ script.js
@@ -2,21 +2,6 @@ let submit = document.querySelector('.add'),
     shelf = document.querySelector('.books-shelf'),
     library = []
 
-function createDiv(title, author, pages) {
-    let element = document.createElement('div'),
-        bookEls = [title, author, pages],
-        names = ['Title', 'Author', 'Pages']
-
-    for (let i = 0; i < 3; i++) {
-        let p = document.createElement('p')
-        p.textContent = `${names[i]}:  ${bookEls[i]}`
-        element.appendChild(p)
-    }
-
-    element.classList.add('book')
-    shelf.appendChild(element)
-}
-
 function Book() {
     this.title = document.querySelector('#title').value
     this.author = document.querySelector('#author').value
@@ -26,8 +11,16 @@ function Book() {
 function addBookToLibrary() {
     let book = new Book;
     library.push(book)
-    createDiv(book.title, book.author, book.pages)
 }
+
+function CreateDiv() {
+
+}
+
+
+CreateDiv.prototype = Object.create(Book.prototype)
+
+
 
 submit.addEventListener('click', addBookToLibrary)
 
